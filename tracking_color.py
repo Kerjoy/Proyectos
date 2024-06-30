6afc030d61939ed22c4146dc574e73e2c0bc5ae7
JC <jc_sr31@outlook.com>
2024-06-30 07:41:07 -0600

    mod without args

--- tracking_color.py
+++ tracking_color.py
@@ -2,42 +2,34 @@
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
-import argparse
 import cv2
 import imutils
 import time
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=32,
-	help="max buffer size")
-args = vars(ap.parse_args())
+
+
+default = 32
+direction = ""
+
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space
 greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
+
 # initialize the list of tracked points, the frame counter,
 # and the coordinate deltas
-pts = deque(maxlen=args["buffer"])
+pts = deque(maxlen=default)
 counter = 0
 (dX, dY) = (0, 0)
-direction = ""
-# if a video path was not supplied, grab the reference
+
 # to the webcam
-if not args.get("video", False):
-	vs = VideoStream(src=0).start()
-# otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
-# allow the camera or video file to warm up
+vs = VideoStream(src=0).start()
 time.sleep(2.0)
+
 # keep looping
 while True:
 	# grab the current frame
 	frame = vs.read()
 	# handle the frame from VideoCapture or VideoStream
-	frame = frame[1] if args.get("video", False) else frame
 	# if we are viewing a video and we did not grab a frame,
 	# then we have reached the end of the video
 	if frame is None:
@@ -107,7 +99,7 @@ while True:
 				direction = dirX if dirX != "" else dirY
     		# otherwise, compute the thickness of the line and
 		# draw the connecting lines
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		thickness = int(np.sqrt(default / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 	# show the movement deltas and the direction of movement on
 	# the frame
@@ -118,16 +110,13 @@ while True:
 		0.35, (0, 0, 255), 1)
 	# show the frame to our screen and increment the frame counter
 	cv2.imshow("Frame", frame)
+	cv2.imshow("Mask", mask)
 	key = cv2.waitKey(1) & 0xFF
 	counter += 1
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
 		break
-# if we are not using a video file, stop the camera video stream
-if not args.get("video", False):
-	vs.stop()
-# otherwise, release the camera
-else:
-	vs.release()
-# close all windows
+
+vs.stop()
+
 cv2.destroyAllWindows()
